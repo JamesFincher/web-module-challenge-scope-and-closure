e27ad1138e49d66695b26d4678baa44588c41ff3
heartandkarma <heartandkarma@gmail.com>
2022-01-15 20:36:35 -0800

    mvp

--- index.js
+++ index.js
@@ -12,11 +12,11 @@
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
 */
-
+const testArray = ['one', 'two', 'three', 'four', 'five']
 function processFirstItem(stringList, callback) {
   return callback(stringList[0])
 }
-console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(processFirstItem(testArray, function (str) { return str + str }));
 
 // ⭐️ Example Challenge END ⭐️
 
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter 1 is a higher function, and return function counter is a callback function.
   2. Which of the two uses a closure? How can you tell?
-  
+  counter 1 return count uses it's parent value count which is out of its scope. A closure is a when a var is passed from a parent function.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -39,7 +39,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
+    return count++;
   }
 }
 
@@ -56,16 +56,16 @@ function counter2() {
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
-  
-  For example: invoking inning() should return a numerical score value of 0, 1, or 2
+    For example: invoking inning() should return a numerical score value of 0, 1, or 2
   
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+  return Math.floor(Math.random() * 3)
+  /*Code Here*/
 }
-
+// console.log(inning())
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -79,21 +79,39 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inning, num) {
+  let returnFinalScore = { Home: 0, Away: 0 }
+  for (let i = 0; i < num; i++) {
+    returnFinalScore.Home = returnFinalScore.Home + inning()
+    returnFinalScore.Away = returnFinalScore.Away + inning()
+  }
+  return returnFinalScore
 }
+
+
+// let test = finalScore(inning, 5)
+// console.log(test)
+// console.log(finalScore(inning, 8))
+// console.log(scoreboard.Home)
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(cb) {
+  let returnInning = { Home: 0, Away: 0 }
+
+  returnInning.Away = inning()
+
+  returnInning.Home = inning()
+  return returnInning  /*Your Code Here */
 }
 
+console.log(getInningScore(inning))
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -144,7 +162,7 @@ function scoreboard(/* CODE HERE */) {
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
+function foo() {
   console.log('its working');
   return 'bar';
 }
